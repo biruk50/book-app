8d28d0ba9da1cb722fb46c25c0e7375cc2bbae59
biruk50 <89568667+biruk50@users.noreply.github.com>
2025-09-06 16:29:24 +0300

    feat: Enhance BookPage with back button and styling; remove unused files

--- app/books/[id]/page.js
+++ app/books/[id]/page.js
@@ -1,12 +1,13 @@
 "use client";
 
 import { books } from "@/constants/mockData";
-import { useParams } from "next/navigation";
+import { useParams, useRouter } from "next/navigation";
 import React, { useState, useEffect } from "react";
 import { ReactReader } from "react-reader";
 
 export default function BookPage() {
   const { id } = useParams();
+  const router = useRouter();
   const selected = books.find((book) => id === String(book.id));
 
   // hide global header/footer while this page is mounted
@@ -28,7 +29,18 @@ export default function BookPage() {
   const [location, setLocation] = useState(null);
   return (
     <div style={{ height: "100vh" }}>
-      <h1 style={{ textAlign: "center" }}>{selected.title}</h1>
+      <div style={headerStyle}>
+        <button className="book-page-back-button" style={buttonStyle}
+          onClick={() => router.push("/")}
+        >
+          <i className="fa fa-arrow-left" style={{ fontSize: "1rem" }}></i>
+        </button>
+
+        <h1 style={titleStyle}>
+          {selected.title}
+        </h1>
+      </div>
+
       <ReactReader
         url={"https://react-reader.metabits.no/files/alice.epub"}
         location={location}
@@ -36,4 +48,25 @@ export default function BookPage() {
       />
     </div>
   );
+}
+
+const headerStyle = {
+          display: "flex",
+          backgroundColor: "#e6e4e4ff",
+          padding: "0.5rem 2rem",
+  }
+const buttonStyle  = {
+          width: "40px",
+          border: "none",
+          borderRadius: "7px",
+          cursor: "pointer",
+      
+}
+
+const titleStyle = {
+          fontSize: "1.2rem",
+          marginBottom: "0.5rem",
+          fontWeight: "bold",
+          textAlign: "center",
+          flex: 1,
 }
